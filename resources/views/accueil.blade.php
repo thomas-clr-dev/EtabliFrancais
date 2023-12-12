b380d3b92568bdc4dca0af21dbbe4a5694c12da1
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2023-12-12 14:17:20 +0100

    Ajout des images accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/accueil.css') }}">
+    <link rel="stylesheet" href="https://use.typekit.net/nep6jxu.css">
+    <link rel="stylesheet" href="https://use.typekit.net/nep6jxu.css">
     <title>Etabli Français</title>
 </head>
 
 <body>
-    <div class="nav">
+    <div class="nav" style="background-image: url({{ asset('img/header.png') }});">
         <div class="colonne1">
-            <img class="Logo" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Logo\Logo.png">
+            <img class="Logo" src="{{ asset('img/logo.png') }}">
         </div>
         <div class="colonne2">
             <a href="#accueil">Accueil</a>
@@ -19,18 +21,14 @@
             <a href="#Contact">Contact</a>
         </div>
         <div class="colonne3">
-            <img class="compte" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Icone1.png">
-            <img class="panier" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Icone2.png">
+            <img class="compte" src="{{ asset('img/profil.png') }}">
+            <img class="panier" src="{{ asset('img/cart.png') }}">
         </div>
     </div>
 
     <div class="header">
         <h1>Nos Domaines</h1>
     </div>
-
-
-
-
 </body>
 
 </html>
