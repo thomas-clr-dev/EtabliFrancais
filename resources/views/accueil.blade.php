7b6142aaf8b7e9af95ad0107b9acc80cf6848f49
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2023-12-12 11:38:40 +0100

    Avancement page d'accueil

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/accueil.css') }}">
+    <title>Etabli Français</title>
+</head>
+
+<body>
+    <div class="nav">
+        <div class="colonne1">
+            <img class="Logo" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Logo\Logo.png">
+        </div>
+        <div class="colonne2">
+            <a href="#accueil">Accueil</a>
+            <a href="#nosartisans">Nos artisans</a>
+            <a href="#boutique">Boutique</a>
+            <a href="#Contact">Contact</a>
+        </div>
+        <div class="colonne3">
+            <img class="compte" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Icone1.png">
+            <img class="panier" src="C:\Users\belle\Documents\MDS\PARTIEL\Images\Icone2.png">
+        </div>
+    </div>
+
+    <div class="header">
+        <h1>Nos Domaines</h1>
+    </div>
+
+
+
+
+</body>
+
+</html>
