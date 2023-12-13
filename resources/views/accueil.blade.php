e019e0eaaf85963f1fa6a7e30d7b817d20931097
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2023-12-13 13:31:23 +0100

    avancement accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -28,31 +28,36 @@
                 <a href=""><img class="panier" src="{{ asset('img/cart.png') }}"></a>
             </div>
         </div>
-
     </div>
+    <div style="background-image: url({{ asset('img/forest.png') }});">
+        <span class="border"></span>
+        <div class="white-filter">
+            <div class="header">
+                <h1>Nos Domaines</h1>
+            </div>
 
-    <div class="header">
-        <h1>Nos Domaines</h1>
-    </div>
+            <div class="métiers">
+                <div>
+                    <img class="bvm" src="{{ asset('img/wood.png') }}">
+                    <h2>Ebénisterie</h2>
+                </div>
+                <div>
+                    <img class="bvm" src="{{ asset('img/glass.png') }}">
+                    <h2>Verrerie</h2>
+                </div>
+                <div>
+                    <img class="bvm" src="{{ asset('img/iron.png') }}">
+                    <h2>Ferronnerie</h2>
+                </div>
+            </div>
 
-    <div class="métiers">
-        <div>
-            <img class="bvm" src="{{ asset('img/wood.png') }}">
-            <h2>Ebénisterie</h2>
+            <div class="btn-s1">
+                <button class="CTA">Découvrez nos artisans</button>
+            </div>
         </div>
-        <div>
-            <img class="bvm" src="{{ asset('img/glass.png') }}">
-            <h2>Verrerie</h2>
-        </div>
-        <div>
-            <img class="bvm" src="{{ asset('img/iron.png') }}">
-            <h2>Ferronnerie</h2>
-        </div>
+
     </div>
 
-    <div class="logo2">
-        <img src="{{ asset('img/logo-large.png') }}">
-    </div>
 
     <div class="fond">
         <h2>Qu'est ce que l'établi francais?</h2>
